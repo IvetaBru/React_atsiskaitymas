fcee4adc23a42e17ce6c85b734c0cbf7066ff033
IvetaBru <ivetamarkeviciute@gmail.com>
2025-04-17 11:59:43 +0300

    footer created and styled

--- react_atsiskaitymas/src/components/outlets/MainOutlet.tsx
+++ react_atsiskaitymas/src/components/outlets/MainOutlet.tsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router";
+import styled from "styled-components";
 import Header from "../UI/organisms/Header";
 import Footer from "../UI/organisms/Footer";
+
+const StyledMain = styled.div`
+    height: calc(100vh - 80px - 200px);
+    padding: 20px 300px;
+`
 
 const MainOutlet = () => {
     return ( 
         <>
             <Header />
-            <main>
+            <StyledMain>
                 <Outlet />
-            </main>
+            </StyledMain>
             <Footer />
         </>
 
