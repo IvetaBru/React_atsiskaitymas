c3686578874e7f4b2bc066462f4588eaafcc207d
IvetaBru <ivetamarkeviciute@gmail.com>
2025-04-17 10:44:44 +0300

    footer, header, outlet, login, register, home files created, routes made in App.tsx, Browserouter added in main.tsx

--- react_atsiskaitymas/src/App.tsx
+++ react_atsiskaitymas/src/App.tsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router";
+import MainOutlet from "./components/outlets/MainOutlet";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+
 
 
 const App = () => {
   
   return (
     <>
-      
+      <Routes>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="" element={<MainOutlet/>}>
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
     </>
   )
 }
