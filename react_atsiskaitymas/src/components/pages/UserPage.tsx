2593112a4da927400796c611bf134fc9950971d4
IvetaBru <ivetamarkeviciute@gmail.com>
2025-04-18 13:12:37 +0300

    loading gif added to user page

--- react_atsiskaitymas/src/components/pages/UserPage.tsx
+++ react_atsiskaitymas/src/components/pages/UserPage.tsx
@@ -15,28 +15,46 @@ const StyledSection = styled.section`
         flex-wrap: wrap;
         gap: 10px;   
     }
+    .loading{
+        height: 100px;
+        width: 100px;
+    }
 `
 
 const UserPage = () => {
 
     const { loggedInUser } = useContext(UsersContext) as UsersContextTypes;
-    const { recipes } = useContext(RecipesContext) as RecipesContextTypes; 
+    const { recipes, isLoading } = useContext(RecipesContext) as RecipesContextTypes; 
 
     const saved = recipes.filter((recipe) =>
         loggedInUser?.savedRecipes.includes(recipe.id)
     );
+    const isEmpty = saved.length === 0;
 
     return ( 
         <StyledSection>
             <h2>Saved recipes</h2>
             <div>
-                {
-                saved.length ? (
-                    saved.map((recipe) => (
-                        <RecipeCard key={recipe.id} data={recipe} />
-                    ))
-                ) : (<p>No saved recipes yet</p>)
-                }
+            {
+                isLoading && (
+                    <div>
+                        <img src="https://i.gifer.com/ZZ5H.gif" alt="loading" className="loading"/>
+                    </div>
+                )
+            }
+            {
+                !isLoading && isEmpty && (
+                    <p>No recipes yet</p>
+                )
+            }
+            {
+                !isLoading && !isEmpty && saved.map(recipe => (
+                    <RecipeCard 
+                    data={recipe}
+                    key={recipe.id}
+                    />
+                ))
+            }
             </div>
         </StyledSection>
      );
